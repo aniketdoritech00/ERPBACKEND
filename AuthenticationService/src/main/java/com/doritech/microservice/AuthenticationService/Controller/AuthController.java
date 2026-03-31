1f72946486c9bd174e6c9685caedbb7763645b4b
aniketdoritech00 <aniket@doritech.in>
2026-03-31 16:25:14 +0530

    remove the comment

--- AuthenticationService/src/main/java/com/doritech/microservice/AuthenticationService/Controller/AuthController.java
+++ AuthenticationService/src/main/java/com/doritech/microservice/AuthenticationService/Controller/AuthController.java
@@ -22,7 +22,6 @@ public class AuthController {
 
 	@PostMapping("/login")
 	public ResponseEntity login(@Valid @RequestBody LoginRequest request) {
-		System.out.println("Login request received for email");
 		return authService.login(request);
 	}
 
