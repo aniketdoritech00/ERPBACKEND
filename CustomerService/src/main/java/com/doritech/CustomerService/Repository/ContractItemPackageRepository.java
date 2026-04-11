53a20a3dd5aca7ea0fb7188422171a7723411016
PrafullDoritech <prafull@doritech.in>
2026-04-11 10:23:50 +0530

    latest change

--- CustomerService/src/main/java/com/doritech/CustomerService/Repository/ContractItemPackageRepository.java
+++ CustomerService/src/main/java/com/doritech/CustomerService/Repository/ContractItemPackageRepository.java
@@ -76,27 +76,30 @@ public interface ContractItemPackageRepository extends JpaRepository<ContractIte
 			""", nativeQuery = true)
 	Optional<ContractItemPackageProjection> findDetailsById(@Param("id") Integer id);
 
-	
-	 @Query(value = """
-	            SELECT
-	                cip.package_id              AS packageId,
-	                cip.contract_mapping_id     AS contractMappingId,
-	                cip.mapped_item_id          AS mappedItemId,
-	                cip.is_active               AS isActive,
-	                cmast.contract_id           AS contractId,
-	                cmast.contract_name         AS contractName,
-	                cmast.contract_no           AS contractCode,
-	                im1.item_name               AS mappingItemName,
-	                im2.item_name               AS mappedItemName,
-	                im2.base_price              AS basePrice,
-	                cm.quantity                 AS qty
-	            FROM  contract_item_package  cip
-	            JOIN  contract_item_mapping  cm    ON cm.contract_mapping_id = cip.contract_mapping_id
-	            JOIN  contract_master        cmast ON cmast.contract_id      = cm.contract_id
-	            JOIN  item_master            im1   ON im1.item_id            = cm.item_id
-	            JOIN  item_master            im2   ON im2.item_id            = cip.mapped_item_id
-	            WHERE cm.contract_id = :contractId
-	            """, nativeQuery = true)
-	    List<ContractItemPackageProjection> findAllByContractId(@Param("contractId") Integer contractId);
+	@Query(value = """
+			SELECT
+			    cip.package_id              AS packageId,
+			    cip.contract_mapping_id     AS contractMappingId,
+			    cip.mapped_item_id          AS mappedItemId,
+			    cip.is_active               AS isActive,
+			    cmast.contract_id           AS contractId,
+			    cmast.contract_name         AS contractName,
+			    cmast.contract_no           AS contractCode,
+			    im1.item_name               AS mappingItemName,
+			    im1.item_code               AS mappingItemCode,
+			    im2.item_name               AS mappedItemName,
+			    im2.item_code               AS mappedItemCode,
+			    im2.base_price              AS basePrice,
+			    cm.quantity                 AS qty
+
+			FROM  contract_item_package  cip
+			JOIN  contract_item_mapping  cm    ON cm.contract_mapping_id = cip.contract_mapping_id
+			JOIN  contract_master        cmast ON cmast.contract_id      = cm.contract_id
+			JOIN  item_master            im1   ON im1.item_id            = cm.item_id
+			JOIN  item_master            im2   ON im2.item_id            = cip.mapped_item_id
+			WHERE cm.contract_id = :contractId
+			""", nativeQuery = true)
+	List<ContractItemPackageProjection> findAllByContractId(@Param("contractId") Integer contractId);
+
 
 }
